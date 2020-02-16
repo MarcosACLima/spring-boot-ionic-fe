0bd90c26ecc8aec4c478a611b9f33a5e875c754b
Marcos Lima <marcosanthony21@hotmail.com>
2020-02-16 00:05:06 -0300

    Mostrar as categorias e imagens salvas no bucket aws

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -9,12 +11,16 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  bucketUrl : string = API_CONFIG.bucketBaseUrl;
+
+  itens : CategoriaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
     this.categoriaService.listarTudo().subscribe(resposta => {
-        console.log(resposta);
+        this.itens = resposta;
     },  
         erro => {
           console.log(erro);
