a654b9a81eb5e194c88fdb5d514e41c0c6f804f5
Marcos Lima <marcosanthony21@hotmail.com>
2020-02-19 11:38:18 -0300

    Tratamento erro 401 e tratamento  padrao de outros erros com Alert
    Erro  401 -  autenticacao

--- src/interceptors/erro-interceptor.ts
+++ src/interceptors/erro-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Injectable()
 export class ErroInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertaCtrl: AlertController) {
     }
 
     intercept(requisicao : HttpRequest<any>, proximo: HttpHandler): Observable<HttpEvent<any>> {
@@ -25,17 +26,35 @@ export class ErroInterceptor implements HttpInterceptor {
             console.log(erroObj);
 
             switch(erroObj.status) {
+                case 401:
+                    this.tratar401();
+                    break;
+
                 case 403:
                     this.tratar403();
                     break;
+
+                default:
+                    this.tratarErroPadrao(erroObj);
             }
 
             return Observable.throw(error);
         }) as any;
     }
-
+    
     tratar403() {
         this.storage.setUsuarioLogado(null);
+    }
+    
+    tratar401() {
+        let alerta = this.alertaCtrl.create({title: 'Erro 401: falha de autenticação', message: 'Email ou senha incorretos', enableBackdropDismiss: false,
+        buttons: [{text: 'OK'}]});
+        alerta.present();
+    }
+
+    tratarErroPadrao(erroObj) {
+        let alerta = this.alertaCtrl.create({title: 'Erro ' + erroObj.status + ': ' + erroObj.error, message: erroObj.message, enableBackdropDismiss: false, buttons: [{text: 'OK'}]});
+        alerta.present();
     }
 
 }
