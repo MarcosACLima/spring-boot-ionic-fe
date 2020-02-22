e2da74d1494f1a4ef53794ec6f276a698d645458
Marcos Lima <marcosanthony21@hotmail.com>
2020-02-22 10:42:29 -0300

    Carregando imagens dos produtos pela url do bucket

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -19,8 +20,19 @@ export class ProdutosPage {
     let categoria_id = this.navParams.get('categoria_id');
     this.produtoService.buscarPorCategoria(categoria_id).subscribe( resposta => {
       this.itens = resposta['content'];
+      this.carregarImagemUrls();
     },
     error => {})
+  }
+
+  carregarImagemUrls() {
+    for(var i = 0; i < this.itens.length; i++) {
+      let item = this.itens[i];
+      this.produtoService.retornarImagemPequenDoBucket(item.id).subscribe(resposta => 
+        { item.imagemUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+      error => {});
+    }
   }
 
 }
